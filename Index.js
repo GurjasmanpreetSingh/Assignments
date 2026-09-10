4eedcf834fc6b51334161ea193dc7ccf83cb9a80
GurjasmanpreetSingh <sgurjasmanpreet@gmail.com>
2026-09-10 13:47:18 +0530

    if-else Assignment

---
+++ Index.js
@@ -0,0 +1,20 @@
+// Q1 What is Operator in javaScript
+// Ans1 - Operator in javascript is used to perform operations in the code 
+
+// Q2 Write a program to add two numbers.
+// Ans2 - 
+let sum = 2 + 3;
+console.log("Addition = " + sum);
+
+// Q3 Use * Operator to multiply two numbers.
+// Ans3 - 
+let num = 2 * 3;
+console.log("Multiplication = "+ num);
+
+// Q4 What is difference between == and === ?
+// Ans4 - 
+
+// Q5 Use the modulo operator(%) to find the remainder of 10/3
+//  Ans5 - 
+let number = 10 % 3;
+console.log("Remainder")
